cb08f11cec575e418a142d44bf6640f7c9416217
NuriaLibertad <nurialibertad29@gmail.com>
2023-05-18 11:05:02 -0300

    entendi mais ou menos

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args){
     //Aula introdutória de Orientação à Objetos
+        //Declaração de objeto
+        Pessoa adao;
+        //Intsancia do objeto
+        adao = new Pessoa();
+        // Ao mesmo tempo
+        Pessoa qualquer = new Pessoa ();
+        // Definir formato do objeto
+        qualquer.nome = "Rita";
+                qualquer.sobrenome = "lee";
+                qualquer.falar();
+                System.out.println(qualquer.falar("oi"));
+    }
+
+    public void criarPessoa(){
 
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,14 @@
+public class Pessoa {
+     //atributos da classe
+     public String nome;
+     public String sobrenome;
+    //métodos da classe
+    public void falar(){
+    System.out.println("falei");
+
+ }
+   public String falar(String texto){
+        return texto;
+}
+
+}
