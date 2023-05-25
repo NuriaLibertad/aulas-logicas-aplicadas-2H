606c8446efa07ae28790a64695a1f6db179cf663
NuriaLibertad <nurialibertad29@gmail.com>
2023-05-25 11:06:02 -0300

    retomada de OO sobscrita

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,33 @@
 public class Main {
-    public static void main(String[] args){
-    //Aula introdutória de Orientação à Objetos
+    public static void main(String[] args) {
+        //Aula introdutória de Orientação à Objetos
         //Declaração de objeto
         Pessoa adao;
         //Intsancia do objeto
         adao = new Pessoa();
         // Ao mesmo tempo
-        Pessoa qualquer = new Pessoa ();
+        Pessoa qualquer = new Pessoa();
         // Definir formato do objeto
         qualquer.nome = "Rita";
-                qualquer.sobrenome = "lee";
-                qualquer.falar();
-                System.out.println(qualquer.falar("oi"));
+        qualquer.sobrenome = "lee";
+        // comportamento
+        qualquer.falar();
+        System.out.println(qualquer.falar("oi"));
+        //Criar novos objetos
+        //Declarar objeto
+        Pessoa rainha;
+        // Instanciar objetos
+        rainha = new Pessoa();
+//Definir forma do objeto
+        rainha.nome = "Tinha";
+        rainha.sobrenome = "Tunner";
+        //Definição do comportamneto
+        rainha.falar();
+        rainha.comer();
+
+
     }
 
-    public void criarPessoa(){
-
-    }
 }
 
 
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,6 +1,6 @@
-public class Pessoa {
+public class Pessoa extends Animal {
      //atributos da classe
-     public String nome;
+
      public String sobrenome;
     //métodos da classe
     public void falar(){
@@ -10,5 +10,11 @@ public class Pessoa {
    public String falar(String texto){
         return texto;
 }
+
+
+    //sobrescrita do método
+    public void comer(){
+        System.out.println("pessoa comeu");
+    }
 
 }
